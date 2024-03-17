c4be88ecf7484fa7569bb10216c5b76b571912fc
Sherry <sherry.4376@gmail.com>
2024-03-17 11:23:04 +0800

    Site updated: 2024-03-17 11:23:03

--- js/script.js
+++ js/script.js
@@ -182,4 +182,25 @@ const getRealPath = (pathname, desc = false) => {
       link.className = "sidebar-menu-link-wrap link-active";
     }
   }
+  firework({
+  excludeElements: ["a"],
+  particles: [
+    {
+      shape: "circle",
+      move: "emit",
+      easing: "easeOutExpo",
+      colors: [
+        "rgba(255,182,185,.9)",
+        "rgba(250,227,217,.9)",
+        "rgba(187,222,214,.9)",
+        "rgba(138,198,209,.9)",
+      ],
+      number: 30,
+      duration: [1200, 1800],
+      shapeOptions: {
+        radius: [16, 32],
+      },
+    },
+  ]
+});
 })(jQuery);

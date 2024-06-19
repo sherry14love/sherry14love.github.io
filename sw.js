6648cab6095fd6148a18e143283d7d96a38cc96a
Sherry <sherry.4376@gmail.com>
2024-06-19 14:58:40 +0800

    Site updated: 2024-06-19 14:58:40

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1718780110565";
+const VERSION = "1718780315657";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",

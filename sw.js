e40be3a0e4a81eee19bfa2abdb21b79c49c8ae24
Sherry <sherry.4376@gmail.com>
2024-06-20 18:47:31 +0800

    Site updated: 2024-06-20 18:47:31

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1718803089384";
+const VERSION = "1718880446686";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",

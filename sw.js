c159f5bbbf943be9b4fbba47fb73ec3562248006
Sherry <sherry.4376@gmail.com>
2025-04-11 10:51:48 +0800

    Site updated: 2025-04-11 10:51:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1742544130053";const preCache = ["/images/taichi.png","https://img.6677677.xyz/file/1734873534230_0.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1744339903593";const preCache = ["/images/taichi.png","https://img.6677677.xyz/file/1734873534230_0.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
